0d658dcb6336e4ace50dff0f658e2ab33a0ffab8
Andrew Porter <mephisto83@hotmail.com>
2016-01-19 14:49:45 +0100

    updates
    updates

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+'use strict'
 
-var React = require('react-native');
+
+import React from 'react-native';
+import FieldForce from './native/android/app';
+import {DEFAULT_URL} from './native/globals';
+import {service} from './src/service/serviceCaller'
+
+ 
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  AppRegistry
 } = React;
 
-var FieldForce = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+service.setDomain(DEFAULT_URL);
 
 AppRegistry.registerComponent('FieldForce', () => FieldForce);
